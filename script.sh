2940c31a2ba0ef3d024c3f374b30e10c069fc9d4
Meng Tian mac <tianmeng94@hotmail.com>
2015-09-21 00:58:14 +0100

    provisioning xdebug.

--- script.sh
+++ script.sh
@@ -5,6 +5,14 @@ apt-get update
 echo "###### Upgrade php and install php modules ######"
 apt-get -y install php5-xsl php5-xdebug
 
+echo "###### Configure xdebug ###### "
+cat << EOF | sudo tee -a /etc/php5/mods-available/xdebug.ini
+xdebug.max_nesting_level = 400
+xdebug.remote_enable=on
+xdebug.remote_connect_back=on
+xdebug.extended_info=1
+EOF
+
 # remove mysql 5.5 and install mysql 5.6
 echo "###### Install MySQL 5.6 ######"
 apt-get -y remove mysql-server
